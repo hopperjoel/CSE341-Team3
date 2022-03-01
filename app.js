312b1984301274ab651af48982fbaec30b44ebf8
Benshirley300 <benshirley300@gmail.com>
2022-02-28 20:03:27 -0700

    App.js update

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const adminRoutes = require('./routes/admin/adminRoutes');
+const authRoutes = require('./routes/authentication/loginRoutes');
+const shopRoutes = require('./routes/products/productRoutes');
+
+const app = express();
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
+app.use(adminRoutes);
+app.use(authRoutes);
+app.use(shopRoutes);
+
+
+
+app.listen(8080);
