26fb2eb6b04ba93c8d243512743aee0aef30de6a
david doria <dor15004@byui.edu>
2022-02-28 20:07:52 -0700

    Missed some semicolons

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const shopRoutes = require('./routes/products/productRoutes');
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-})
+});
 
 app.use(adminRoutes);
 app.use(authRoutes);
