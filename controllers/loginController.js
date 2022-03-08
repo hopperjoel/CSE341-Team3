3c99ef28e2c0fee2347a77c9a27d84e2f53b6cbf
mckenzielew92 <mckenzielew92@yahoo.com>
2022-03-07 19:20:20 -0700

    login and product controllers

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -15,3 +15,42 @@ exports.getLogin = (req, res, next) => {
 /****************************************************
  * POST Controllers
  ****************************************************/
+
+exports.postSignup = (req, res, next) => {
+const email = req.body.email;
+const password = req.body.password;
+const errors = validationResult(req);
+if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+    //path: '/signup',
+    //pageTitle: 'Signup',
+    errorMessage: errors.array()[0].msg,
+    oldInput: {
+        email: email,
+        password: password,
+        confirmPassword: req.body.confirmPassword
+    },
+    validationErrors: errors.array()
+    });
+}
+
+// bcrypt
+//     .hash(password, 12)
+//     .then(hashedPassword => {
+//     const user = new User({
+//         email: email,
+//         password: hashedPassword,
+//         cart: { items: [] }
+//     });
+//     return user.save();
+//     })
+//     .then(result => {
+//     res.redirect('/login');
+//     })
+//     .catch(err => {
+//     const error = new Error(err);
+//     error.httpStatusCode = 500;
+//     return next(error);
+//     });
+};
--- controllers/productController.js
+++ controllers/productController.js
@@ -3,12 +3,36 @@
  * 
  * Controller for the main page of the store
  ***************************************************/
- const Product = require('../models/products');
+//const products = require('../models/products');
+const Product = require('../models/products');
 
 /****************************************************
  * GET Controllers
  ****************************************************/
-
+exports.getHomepage = (req, res, next) => {
+    return products.find() //findById() to get certain items?
+        .then(result => {
+            res.status(200).json({ //add in an error message?
+                message: "Home display products request successful",
+                title: result.title,
+                image: result.image,
+                description: result.description
+                //thinking maybe you don't need the price for the home page display?
+            })
+        })
+};
+    // const product = homeProduct({
+    //     title: title,
+    //     image: image
+    // });
+    // product
+    //     .save()
+    //     .then((result) => {
+    //         res.status(200).json({
+    //           //Do I need to add a message here for get?  message: ''
+    //         })
+    //     }) 
+    
 
 /****************************************************
  * POST Controllers
